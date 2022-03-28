d0ef52734b03625152ecc2eb6889bd6dd1760633
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-28 20:23:13 +0000

    Added script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@
  * This file contains the JS functions for index.html 
 */
 
+'use strict'
+
 /**
- * Do basic math.
+ * This function calculates the area and perimeter of the parallelogram.
  */
+function calculate () {
+  // input
+  const base = parseFloat(document.getElementById('parallelogram-base').value)
+  const height = parseFloat(document.getElementById('parallelogram-height').value)
+
+  // process
+  const area = base * height
+
+  // output
+  document.getElementById('area').innerHTML = 'The Area of the parallelogram is: ' + area + ' cm²'
+}
